82679a4b72745a66af2a79256880419c01ab6e5d
𝓑𝓛𝓐𝓧𝓚 𝓜𝓘𝓡𝓐 <blaxkmira128@gmail.com>
2026-03-25 01:49:16 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center vh-100 bg-light">
+    <LoginForm v-if="!loggedIn" @login-success="loggedIn = true" />
+    <Dashboard v-else />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import LoginForm from './components/LoginForm.vue'
+import Dashboard from './views/Dashboard.vue'
+
+const loggedIn = ref(false)
+</script>
